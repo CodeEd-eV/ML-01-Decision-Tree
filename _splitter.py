cb0ed71dc96eb9ec8f505258ee3f1f880eef8131
Frank <frankz.lu@gmail.com>
2019-03-27 19:45:45 +0100

    split data

---
+++ _splitter.py
@@ -0,0 +1,14 @@
+from sklearn.model_selection import train_test_split
+import tools.extract as ex
+import os
+
+def split(filename):
+    return train_test_split(ex.load_file(filename))
+
+def write_split(train, test, DIR):
+    train.to_csv( os.path.join(DIR, "train.csv"), sep=",", encoding="utf-8", index=False) 
+    test.to_csv( os.path.join(DIR, "test.csv"), sep=",", encoding="utf-8", index=False) 
+    
+if __name__ == "__main__" :
+    train, test = split("data/adult/data.csv")
+    write_split(train, test, "data/adult")
---
+++ tools/extract.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+
+def load(data_dir):
+    train = pd.read_csv(os.path.join(data_dir, "train.csv"))
+    test = pd.read_csv(os.path.join(data_dir, "test.csv"))
+    return train, test
+
+def load_train(data_dir):
+    return pd.read_csv(os.path.join(data_dir, "train.csv"))
+
+def load_file(filename):
+    return pd.read_csv(filename)
+
+
